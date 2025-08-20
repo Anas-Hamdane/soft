da295dcaff3fe113486d44fda06b97299f0ebf8a
Anas Hamdane <anasdahi44@gmail.com>
2025-08-20 23:04:47 +0100

    bug fix in `todo` and `unreachable` implementations

--- src/common.cpp
+++ src/common.cpp
@@ -4,12 +4,12 @@
 namespace soft {
   void __todo__impl(const char* file, int line, const char* func) {
     std::println(stderr, "`todo()` call:");
-    std::println(stderr, "feature not yet implemented in function %s at %s:%d", func, file, line);
+    std::println(stderr, "feature not yet implemented in function {} at {}:{}", func, file, line);
     std::abort();
   }
 
   void __unreachable__impl(const char* file, int line, const char* func) {
-    std::println(stderr, "`unreachable()` call in function %s at %s:%d", func, file, line);
+    std::println(stderr, "`unreachable()` call in function {} at {}:{}", func, file, line);
     std::abort();
   }
 }
